b50f8496ad3ead0d06c2533914a38e2ee3138d88
GermanA <armendariz.german@gmail.com>
2024-08-25 21:24:12 -0600

    Bootstrap

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
+
+
+const boxes2 = document.querySelectorAll(".box");
+
+window.addEventListener("scroll", () => {
+
+
+  const triggerBottom = window.innerHeight;
+
+  boxes2.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+
+
+  });
+});
+
+const boxes = document.querySelectorAll('.box');
+
+function checkScroll() {
+  boxes.forEach(box => {
+    const boxTop = box.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+    if (boxTop < windowHeight * 0.75) {
+      box.classList.add('visible');
+    } else {
+      box.classList.remove('visible');
+    }
+  });
+}
+
+window.addEventListener('scroll', checkScroll);
+checkScroll(); // Para comprobar el estado inicial al cargar la página
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('myForm');
   
--- views/index.view.php
+++ views/index.view.php
@@ -4,123 +4,132 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Desarrollo Web - </title>
-    <link rel="stylesheet" href="estilos/styles.css">
+    <title>Desarrollo Web</title>
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="estilos/styles.css">
 </head>
 
-<body class="bg-primary-subtle">
+<body>
 
-    <div class="container">
+    <div class="container text-center">
 
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">GA Tech</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                        </li>
-
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Servicios
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Portafolio</a>
+                            <a class="nav-link" href="#">Features</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Nosotros</a>
+                            <a class="nav-link" href="#">Pricing</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Contacto</a>
+                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
                         </li>
-
                     </ul>
-                    <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Search</button>
-                    </form>
                 </div>
             </div>
         </nav>
 
 
-        <section class="hero">
-            <div class="container">
+        <div class="row card text-center box">
+            <div class="card-body">
                 <h1>Desarrollo Web de Alta Calidad</h1>
                 <p>Potencia tu negocio con soluciones web innovadoras y personalizadas.</p>
-                <a href="#contact-form" class="btn-primary">Contáctanos</a>
-            </div>
-        </section>
 
-        <section class="features">
-            <div class="container">
-                <div class="feature">
-                    <h2>Soluciones Personalizadas</h2>
-                    <p>Adaptamos nuestras soluciones a las necesidades específicas de tu negocio.</p>
-                </div>
-                <div class="feature">
-                    <h2>Diseño Responsivo</h2>
-                    <p>Garantizamos una experiencia de usuario óptima en cualquier dispositivo.</p>
-                </div>
-                <div class="feature">
-                    <h2>Soporte Continuo</h2>
-                    <p>Brindamos soporte técnico y mantenimiento constante para asegurar el funcionamiento óptimo de tus aplicaciones.</p>
-                </div>
-            </div>
-        </section>
+                <a href="#myForm" type="button" class="btn btn-primary btn-lg">Contáctanos</a>
 
-        <section class="contact" id="contact-form">
-            <div class="container">
-                <h2>Solicita una Consulta Gratuita</h2>
-                <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" id="myForm" method="POST" class="">
-                    <div class="form-group">
-                        <label for="name">Nombre</label>
-                        <input type="text" id="name" name="name" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Correo Electrónico</label>
-                        <input type="email" id="email" name="email" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="phone">Teléfono</label>
-                        <input type="tel" id="phone" name="phone" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="message">Mensaje</label>
-                        <textarea id="message" name="message" required></textarea>
-                    </div>
-                    <button type="submit" class="btn-primary" name="action">Enviar</button>
-                </form>
             </div>
-        </section>
 
-        <footer>
-            <div class="container">
-                <p>&copy; 2024 Empresa XYZ. Todos los derechos reservados.</p>
+        </div>
+
+
+        <div class="row box">
+            <div class="col card">
+                <h2>Soluciones Personalizadas</h2>
+                <p>Adaptamos nuestras soluciones a las necesidades específicas de tu negocio.</p>
             </div>
-        </footer>
+            <div class="col card">
+                <h2>Diseño Responsivo</h2>
+                <p>Garantizamos una experiencia de usuario óptima en cualquier dispositivo.</p>
+            </div>
+            <div class="col card">
+                <h2>Soporte Continuo</h2>
+                <p>Brindamos soporte técnico y mantenimiento constante para asegurar el funcionamiento óptimo de tus aplicaciones.</p>
+            </div>
+        </div>
+
+
+
+
+
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" id="myForm" method="POST" class="">
+            <div class="row justify-content-md-center box">
+
+                <div class="col-sm-12">
+
+                    <h2>Solicita una Consulta Gratuita</h2>
+                </div>
+
+                <div class="col-sm-12 col-md-10">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Nombre:</label>
+                        <input type="text" class="form-control" id="name" name="name" required>
+                    </div>
+
+                </div>
+
+
+
+                <div class="col-sm-12 col-md-10">
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Correo Electrónico:</label>
+                        <input type="email" class="form-control" id="email" name="email" required>
+                    </div>
+                </div>
+
+                <div class="col-sm-12 col-md-10">
+                    <div class="mb-3">
+                        <label for="phone" class="form-label">Teléfono:</label>
+                        <input type="tel" class="form-control" id="phone" name="phone" required>
+                    </div>
+                </div>
+
+                <div class="col-sm-12 col-md-10">
+                    <div class="mb-3">
+                        <label for="message" class="form-label">Mensaje:</label>
+                        <textarea id="message" class="form-control" name="message" required></textarea>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-10"><button type="submit" class="btn btn-primary" name="action">Enviar</button>
+                </div>
+            </div>
+        </form>
+
+
+        <div class="row box">
+            <footer>
+                <p>&copy; GA Tech. Todos los derechos reservados.</p>
+            </footer>
+        </div>
+
 
     </div>
 
 
 
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
     <script src="js/script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
