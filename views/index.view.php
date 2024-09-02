d48bb645fa4907ee9813488b3c919851a404de98
GermanA <armendariz.german@gmail.com>
2024-09-01 22:01:09 -0600

    Boton WhatsApp

--- views/index.view.php
+++ views/index.view.php
@@ -8,6 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="estilos/styles.css">
+    <link rel="stylesheet" type="text/css" href="fontello-a540fdbf\css\fontello.css">
 </head>
 
 <body>
@@ -151,7 +152,9 @@
 
 
 
-
+        <a class="btn-wsp" target="_blank" href="https://wa.me/525522619648?text=Buen%20día...">
+            <i class="icon-whatsapp"></i>
+        </a>
 
 
 
