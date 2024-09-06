4a38e376e15aa22bc22118f4cb0f73d477ff0ed1
German MSI <armendariz.german@outlook.com>
2024-09-06 07:39:00 -0600

    Navbar &  Estilos

--- views/index.view.php
+++ views/index.view.php
@@ -25,26 +25,14 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link active" aria-current="page" href="#">Inicio</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                            <a class="nav-link" href="#Pricing">Pricing</a>
                         </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
-                                </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
-                            </ul>
-                        </li>
+                        
                         <li class="nav-item">
-                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                            <a class="nav-link" aria-disabled="true" href="#Contacto">Contacto</a>
                         </li>
                     </ul>
                     <form class="d-flex" role="search">
@@ -88,7 +76,7 @@
             </div>
         </div>
 
-        <div class="row box">
+        <div class="row box" id="Pricing">
             <div class="col-sm-6 col-md-3 card-pricing">
                 <h2 class="text-center">MicroStart</h2>
 
@@ -159,9 +147,9 @@
 
 
         <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" id="myForm" method="POST" class="">
-            <div class="row justify-content-md-center box">
+            <div class="row justify-content-md-center box" id="Contacto">
 
-                <div class="col-sm-12">
+                <div class="col-sm-12 Contacto">
 
                     <h2>Solicita una Consulta Gratuita</h2>
                 </div>
